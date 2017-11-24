c9ceaec08ce1b943dd6ca82d426200fc6f61d319
Vikas Rajoria <vrajoria@walmartlabs.com>
2017-11-25 01:21:38 +0530

    sorting module

--- gforg/src/main/java/com/vraj/playground/gforg/MaxTipCalculator.java
+++ gforg/src/main/java/com/vraj/playground/gforg/MaxTipCalculator.java
@@ -9,44 +9,10 @@ package com.vraj.playground.gforg;
 public class MaxTipCalculator {
 
 	public static void main(String[] args) {
-		int N = 8;
-		int X = 4;
-		int Y = 4;
-		int[] A = { 1, 4, 3, 2, 7, 5, 9, 6 };
-		int[] B = { 1, 2, 3, 6, 5, 4, 9, 8 };
-
-		int maxTip = 0;
-
-		int[][] track = new int[5][5];
-		track[0][0] = 0;
-
-		for (int i = 0; i < N; i++) {
-			switch (compare(A[i], B[i])) {
-			case ">":
-				maxTip = maxTip + A[i];
-				X--;
-				break;
-			case "<":
-				break;
-			case "=":
-				break;
-			}
-		}
+		
 	}
-
-	private static String compare(int i, int j) {
-		// TODO Auto-generated method stub
-		if (i > j) {
-			return ">";
-		}
-
-		if (i < j) {
-			return "<";
-		}
-
-		if (i == j) {
-			return "=";
-		}
-		return "none";
+	
+	public void sort(int[] tips) {
+		
 	}
 }
---
+++ gforg/src/main/java/com/vraj/playground/gforg/sorting/QuickSorting.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.vraj.playground.gforg.sorting;
+
+/**
+ * 
+ * Simple class to demonstrate quick sorting technique.
+ * 
+ * @author vrajori
+ *
+ */
+public class QuickSorting {
+
+	public void partition(int arr[], int start, int end) {
+
+		if (start == end) {
+			return;
+		}
+		int pivotElement = arr[end];
+		int index = start;
+		for (int i = start; i < end; i++) {
+			if (arr[i] < pivotElement) {
+				swap(arr, i, index);
+				index++;
+			}
+		}
+		swap(arr, index, end);
+		partition(arr, start, index - 1);
+		partition(arr, index + 1, end);
+	}
+
+	private void swap(int[] arr, int i, int j) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+
+	public static void main(String[] args) {
+		QuickSorting sort = new QuickSorting();
+		int[] a = { 12, 3, 8, 40, 20, 10, 15 };
+		sort.partition(a, 0, 6);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+}
