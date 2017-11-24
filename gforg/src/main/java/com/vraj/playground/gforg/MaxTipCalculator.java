836664140d188cafa3240e507c5400b37ed743d4
Vikas Rajoria <vrajoria@walmartlabs.com>
2017-11-24 23:07:26 +0530

    Tip problem

---
+++ gforg/src/main/java/com/vraj/playground/gforg/MaxTipCalculator.java
@@ -0,0 +1,19 @@
+package com.vraj.playground.gforg;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/maximum-tip-calculator/0
+ * 
+ * @author vrajori
+ *
+ */
+public class MaxTipCalculator {
+	public static void main(String[] args) {
+		int N = 8;
+		int X = 4;
+		int Y = 4;
+		int[] A = { 1, 4, 3, 2, 7, 5, 9, 6 };
+		int[] B = { 1, 2, 3, 6, 5, 4, 9, 8 };
+
+		int maxTip = 0;
+	}
+}
